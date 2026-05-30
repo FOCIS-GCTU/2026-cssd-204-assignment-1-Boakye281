a16ef8a456b6aebc95b8208c539d1cbae19ce4d7
Boakye281 <2425402656@live.gctu.edu.gh>
2026-05-30 18:09:17 +0000

    Add files via upload

--- initials.py
+++ initials.py
@@ -1,7 +1,32 @@
+# File: initials.py
+# Description: Print out my initials.
+# Assignment: 1
+#
+# NAME: Agyeibi boakye Sekyere
+# STUDENT ID: 2425402656
+# Email: 2425402656@live.gctu.edu.gh
+# Grader: Augustus Buckman
+#
+# On my honor, Agyeibi Boakye Sekyere, this programming assignment is my own work
+#and I have not provided this code to any other student.
 
 def main():
-  pass #code goes here
+    # Print my initials in large block letters
+
+    print()
+    print("...ABS")
+    print()
 
 
+    print("......AA..........BBBBBBBB........SSSSSSSS......")
+    print(".....AA.AA........BB.....BB......SS......SS.....")
+    print("....AA...AA.......BB.....BB......SS.............")
+    print("...AA.....AA......BBBBBBBB.........SSSSSS.......")
+    print("...AAAAAAAAA......BB.....BB............SS......")
+    print("...AA.....AA......BB.....BB......SS....SS.......")
+    print("...AA.....AA......BBBBBBBB.........SSSSSS.......")
+
+    print()
 
 main()
+
